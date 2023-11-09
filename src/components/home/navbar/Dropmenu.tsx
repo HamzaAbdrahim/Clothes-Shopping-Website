8817c93325409d256204e9addc3ab61aa08d50a4
hamza <essh4014@gmail.com>
2023-11-09 08:34:06 +0100

    fix responsive

--- src/components/home/navbar/Dropmenu.tsx
+++ src/components/home/navbar/Dropmenu.tsx
@@ -1,17 +1,25 @@
 import assest from "../../../assets/imges"
 import { bar } from "../../../content"
+import useClickOutside from "../../../hooks/useclickoutside"
 import "../../../scss/pages/home/_navbar.scss"
 
 const Dropmenu = (prop:any) => {
+  const handeltoggol = () => {
+    prop.onclick()
+  }
+
+  const Ref = useClickOutside(handeltoggol)
+
   return (
     <>
     <div className="shadow">
     </div>
-    <div className="bar Dropmenu">
-      <img src={assest.close} alt="close" style={{marginBottom:'1rem' , cursor:"pointer"}} onClick={prop.onclick} />
+    <div ref={Ref} className="bar Dropmenu">
+      <img src={assest.close} alt="close" style={{marginBottom:'1rem' , cursor:"pointer" , height:'2rem'}} onClick={prop.onclick} />
       <ul>
         {bar.map((ele , index) => (
-          <li key={index} className={`bar_element${index}`}>
+          <li key={index} 
+          className={`bar_element${index}`}>
             <h1>{ele.titel}</h1>
             <img src={ele.icon} alt="icon" />
             </li>
